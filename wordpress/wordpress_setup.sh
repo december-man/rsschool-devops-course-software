ccf38f130214a19ba1c86c4d1e8427e8cd0b24ca
december-man <aspasjutin@outlook.com>
2024-11-11 00:35:42 +0300

    update wordpress_setup.sh

--- wordpress/wordpress_setup.sh
+++ wordpress/wordpress_setup.sh
@@ -4,3 +4,4 @@ cd ~
 git clone --branch task_5 https://github.com/december-man/rsschool-devops-course-software.git
 cd rss*/wordpress
 helm install wordpress . --set dbPassword=$WORDPRESS_PASSWORD
+helm list
