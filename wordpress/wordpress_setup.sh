558f49a25c5d7f791c9f7214a591a2e8b955ad10
december-man <aspasjutin@outlook.com>
2024-11-11 00:23:30 +0300

    Commentary, workflow update

--- wordpress/wordpress_setup.sh
+++ wordpress/wordpress_setup.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+sudo yum install git -y
+cd ~
+git clone --branch task_5 https://github.com/december-man/rsschool-devops-course-software.git
+cd rss*/wordpress
+helm install wordpress . --set dbPassword=$WORDPRESS_PASSWORD
