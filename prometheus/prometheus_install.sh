9fe16db40220da051dde4a1f6b26d7a6f6881523
december-man <aspasjutin@outlook.com>
2024-11-30 17:22:06 +0300

    Task-7: Prometheus installation GHA pipeline, prometheus installation shell script

---
+++ prometheus/prometheus_install.sh
@@ -0,0 +1,18 @@
+# Add bitnami helm repo
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+# Add k3s namespace for monitoring utilities
+kubectl create namespace monitoring
+# Install prometheus in the monitoring namespace
+helm upgrade prometheus bitnami/kube-prometheus --install --namespace monitoring \
+  --set service.type=NodePort \
+  --set service.port=32000 \
+  --set service.nodePort=32000
+# Wait for the installation to complete
+sleep 90
+# Check all the prometheus pods for issues
+kubectl get pods -n monitoring
+# Check Monitoring Services
+kubectl get svc -n monitoring
+# Check prometheus UI availability
+curl http://localhost:32000/graph
