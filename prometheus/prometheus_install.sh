4904e75d94435ffae7b98625d5e11157db5942ad
december-man <aspasjutin@outlook.com>
2024-11-30 17:44:48 +0300

    Task_7: prettify install script

--- prometheus/prometheus_install.sh
+++ prometheus/prometheus_install.sh
@@ -2,15 +2,14 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 # Add k3s namespace for monitoring utilities
-kubectl create namespace monitoring
+kubectl create namespace monitoring --dry-run=client -o yaml | kubectl apply -f -
 # Install prometheus in the monitoring namespace
-helm upgrade prometheus bitnami/kube-prometheus \
-  --install \
-  --namespace monitoring \
+helm upgrade prometheus bitnami/kube-prometheus --install --namespace monitoring \
   --set service.type=NodePort \
   --set service.port=32000 \
   --set service.nodePort=32000
 # Wait for the installation to complete
+echo 'Waiting for the installation to complete ...'
 sleep 90
 # Check all the prometheus pods for issues
 kubectl get pods -n monitoring
