c6111c9b35ec7c90972ea7e1740653e92ab71efd
december-man <aspasjutin@outlook.com>
2024-12-06 15:45:52 +0300

    add correct links in install script, fix values.yaml

--- grafana/grafana_install.sh
+++ grafana/grafana_install.sh
@@ -1,9 +1,9 @@
 echo 'Creating Admin Password Secret'
 kubectl create secret generic grafana-admin-pw -n monitoring --from-literal=password="$GRAFANA_PASSWORD" 
 echo 'Grabbing Grafana Helm Chart configuration'
-curl -o ~/grafana_values.yaml https://raw....
+curl -o ~/grafana_values.yaml https://raw.githubusercontent.com/december-man/rsschool-devops-course-software/refs/heads/task_8/grafana/grafana_values.yaml
 echo 'Grabbing Grafana Dashboard .JSON'
-curl -o ~/grafana_dash.json https://raw....
+curl -o ~/grafana_dash.json https://github.com/december-man/rsschool-devops-course-software/blob/task_8/grafana/grafana_dash.json
 kubectl create configmap grafana-dash -n monitoring --from-file=grafana_dash.json
 echo 'Installing Grafana...'
 helm upgrade --install grafana bitnami/grafana -n monitoring -f ~/grafana_values.yaml
