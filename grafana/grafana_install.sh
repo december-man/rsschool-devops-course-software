28b2cd8b1068ea27d7fc21d29a8f6f8ff2978091
december-man <aspasjutin@outlook.com>
2024-12-06 16:50:55 +0300

    prettify

--- grafana/grafana_install.sh
+++ grafana/grafana_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo 'Creating Admin Password Secret'
+echo 'Creating Admin Password Secret...'
 kubectl create secret generic grafana-admin-pw -n monitoring --from-literal=password="$GRAFANA_PASSWORD" 
 
-echo 'Grabbing Grafana Helm Chart configuration'
+echo 'Grabbing Grafana Helm Chart configuration...'
 curl -o ~/grafana_values.yaml https://raw.githubusercontent.com/december-man/rsschool-devops-course-software/refs/heads/task_8/grafana/grafana_values.yaml
 
-echo 'Grabbing Grafana Dashboard .JSON Model'
+echo 'Grabbing Grafana Dashboard .JSON Model...'
 curl -o ~/grafana_dash.json https://github.com/december-man/rsschool-devops-course-software/blob/task_8/grafana/grafana_dash.json
 kubectl create configmap grafana-dash -n monitoring --from-file=grafana_dash.json
 
@@ -13,5 +13,5 @@ echo 'Installing Grafana...'
 helm upgrade --install grafana bitnami/grafana -n monitoring -f ~/grafana_values.yaml
 sleep 90
 
-echo 'Checking installation'
+echo 'Checking installation...'
 kubectl get svc,pods,deployment -n monitoring
