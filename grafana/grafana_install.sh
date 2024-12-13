1347a84a81a34a84e361a4584ad16d9e7b453302
december-man <aspasjutin@outlook.com>
2024-12-13 19:51:50 +0300

    add task-9 updates: updated dashboard, grafana_values.yaml, grafana_install.sh, grafana_setup GHA pipe. Add grafana_alerts config. New screenshots

--- grafana/grafana_install.sh
+++ grafana/grafana_install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-echo 'Creating Admin Password Secret...'
+echo 'Creating Admin Password & SMTP Credentials Secrets...'
 kubectl create secret generic grafana-admin-pw -n monitoring --from-literal=password="$GRAFANA_PASSWORD" 
+kubectl create secret generic grafana-smtp-ses -n monitoring --from-literal=user="$GRAFANA_SMTP_SES_USER" --from-literal=password="$GRAFANA_SMTP_SES_PASSWORD"
 
 echo 'Grabbing Grafana Helm Chart configuration...'
-curl -o ~/grafana_values.yaml https://raw.githubusercontent.com/december-man/rsschool-devops-course-software/refs/heads/task_8/grafana/grafana_values.yaml
+curl -o ~/grafana_values.yaml https://raw.githubusercontent.com/december-man/rsschool-devops-course-software/refs/heads/task_9/grafana/grafana_values.yaml
 
 echo 'Grabbing Grafana Dashboard .JSON Model...'
-curl -o ~/grafana_dash.json https://raw.githubusercontent.com/december-man/rsschool-devops-course-software/refs/heads/task_8/grafana/grafana_dash.json
+curl -o ~/grafana_dash.json https://raw.githubusercontent.com/december-man/rsschool-devops-course-software/refs/heads/task_9/grafana/grafana_dash.json
 kubectl create configmap grafana-dash -n monitoring --from-file=grafana_dash.json
+
+echo 'Setting up Grafana Alerting System...'
+echo 'Setting up Alerts and Email Notifications...'
+curl -o ~/grafana_values.yaml https://raw.githubusercontent.com/december-man/rsschool-devops-course-software/refs/heads/task_9/grafana/grafana_alerts.yaml
+kubectl create configmap grafana-alerts -n monitoring --from-file=grafana_alerts.yaml
 
 echo 'Installing Grafana...'
 helm upgrade --install grafana bitnami/grafana -n monitoring -f ~/grafana_values.yaml
